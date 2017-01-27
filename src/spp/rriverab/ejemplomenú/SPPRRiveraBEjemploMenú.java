661d3b797f974d2079ad75297c4d37891ecbe20c
Roberto <Roberto@Alain>
2017-01-27 09:00:22 -0600

    Ejemplo de menú

--- src/spp/rriverab/ejemplomenú/SPPRRiveraBEjemploMenú.java
+++ src/spp/rriverab/ejemplomenú/SPPRRiveraBEjemploMenú.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Nombre: Roberto Alain Rivera Bravo
+//Matrícula: A01411516
+//Carrera: IMT11
+
 package spp.rriverab.ejemplomenú;
 
 import java.util.Scanner;
-/**
- *
- * @author Roberto
- */
+
 public class SPPRRiveraBEjemploMenú {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         
         Scanner teclado = new Scanner (System.in);
